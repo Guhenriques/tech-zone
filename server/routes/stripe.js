7bb8c2bdc4b382b3652bc17427db7d9b2f3d163e
Gustavo <guhenriques1398@gmail.com>
2023-06-08 14:13:16 -0300

    Stripe checkout implementation

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -8,14 +8,74 @@ const stripe = Stripe(process.env.STRIPE_KEY);
 const router = express.Router();
 
 router.post('/create-checkout-session', async (req, res) => {
+  const line_items = req.body.cartItems.map(item => {
+    return {
+      price_data: {
+        currency: "GBP",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          metadata: {
+            id: item.id
+          }
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.cartQuantity,
+    }
+  })
   const session = await stripe.checkout.sessions.create({
-    line_items: [
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA', 'GB', 'BR'],
+    },
+    shipping_options: [
       {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1NGhl5K5wvHWh3dQk0wbQTyF',
-        quantity: 1,
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 0,
+            currency: 'GBP',
+          },
+          display_name: 'Free shipping',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 3,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 7,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 1500,
+            currency: 'GBP',
+          },
+          display_name: 'Next day air',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 1,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 1,
+            },
+          },
+        },
       },
     ],
+
+    phone_number_collection: {
+      enabled: true,
+    },
+
+    line_items,
     mode: 'payment',
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
     cancel_url: `${process.env.CLIENT_URL}/cart`,
