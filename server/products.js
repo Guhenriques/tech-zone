4f96755988c4c9e94049ee99476afd3a9d511e27
Gustavo <guhenriques1398@gmail.com>
2023-06-19 12:30:19 -0300

    Added new products

--- server/products.js
+++ server/products.js
@@ -1,31 +1,45 @@
 const products = [
   {
     id: 1,
-    name: "Iphone 12 Pro",
+    name: "Iphone 13",
     desc: "6.1 inch display",
-    price: 999,
-    image: "https://res.cloudinary.com/dvteazrgs/image/upload/v1685984087/e-commerce/iphone12pro_elirv7.jpg"
+    price: 699,
+    image: "https://res.cloudinary.com/dvteazrgs/image/upload/v1687170289/apple-iphone-13-starlight-09142021_myqbg2.webp"
   },
   {
     id: 2,
     name: "Iphone 12",
-    desc: "5.4-inch display",
-    price: 699,
-    image: "https://res.cloudinary.com/dvteazrgs/image/upload/v1685984087/e-commerce/iphone12_hfgrbb.jpg"
+    desc: "6.1-inch display",
+    price: 599,
+    image: "https://res.cloudinary.com/dvteazrgs/image/upload/v1687170337/apple-iphone-12-black-10132020_stbu2x.webp"
   },
   {
     id: 3,
-    name: "Galaxy S",
+    name: "Galaxy S22 Plus",
     desc: "6.5-inch display",
-    price: 399,
-    image: "https://res.cloudinary.com/dvteazrgs/image/upload/v1685984087/e-commerce/galaxyS_tdlkkc.png",
+    price: 499,
+    image: "https://res.cloudinary.com/dvteazrgs/image/upload/v1687168351/samsung-s22-green-sku-header-110122_qbcrp1.png",
   },
   {
     id: 4,
+    name: "Galaxy S23",
+    desc: "6.1-inch display",
+    price: "599",
+    image: "https://res.cloudinary.com/dvteazrgs/image/upload/c_scale,w_300/v1687168151/efpk9il4xxc3ytxs0ty0.jpg"
+  },
+  {
+    id: 5,
     name: "MacBook Air",
-    desc: "13-inch with M1 Chip",
+    desc: "13-inch - M1 Chip",
     price: 999,
     image: "https://res.cloudinary.com/dvteazrgs/image/upload/c_scale,w_500/v1687080349/macbook-air-gold-select-201810_eotlo2.jpg"
+  },
+  {
+    id: 6,
+    name: "Ipad 10th Gen",
+    desc: "10.9-inch display",
+    price: 499,
+    image: "https://res.cloudinary.com/dvteazrgs/image/upload/v1687170078/None_6fe5af4c-4dcb-466f-9d11-4612f2194619_udbf00.jpg"
   }
 ]
 
